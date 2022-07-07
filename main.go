9216857a390e0f55f1c9ddd6c5503ad22644a3d9
chelsea benony <chelsea.benony@splice.com>
2022-07-07 13:36:42 -0400

    Adding go file to the repository so we can reach an endpoint

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Welcome to the homepage!")
+	http.ServeFile(w, r, r.URL.Path[1:])
+
+	fmt.Println("Endpoint Hit: homepage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homepage)
+	log.Fatal(http.ListenAndServe(":10100", nil))
+}
+
+func main() {
+	handleRequests()
+}
